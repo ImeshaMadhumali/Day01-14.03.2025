2f9a42c96f27cfc05771cf15d693693ed5866eef
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2025-03-19 11:14:14 +0000

    Question2 completed

--- Question1/one.java
+++ Question1/one.java
@@ -1,6 +1,6 @@
 public class one{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 }
 
@@ -9,4 +9,4 @@ public class one{
 // @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025 (main) $ cd Question1
 // @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question1 (main) $ javac one.java
 // @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question1 (main) $ java one
-// Hello, World!
+// Hello World!
--- Question2/two.java
+++ Question2/two.java
@@ -1,6 +1,6 @@
-public class two {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+public class two{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
             System.out.print(i + " ");
         }
     }
