55105f102d24a81b90cec2c3e95271bb81d43966
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2025-03-19 01:12:43 +0000

    Question1 completed

---
+++ Question1/one.java
@@ -0,0 +1,12 @@
+public class one{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+// Output
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025 (main) $ cd Question1
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question1 (main) $ javac one.java
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question1 (main) $ java one
+// Hello, World!
