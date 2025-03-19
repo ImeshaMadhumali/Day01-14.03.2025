5e53f78807f111f19637f20e4fab7280c7b596fa
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2025-03-19 11:39:23 +0000

    Question4 completed

---
+++ Question4/four.java
@@ -0,0 +1,13 @@
+public class four {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+// Output
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025 (main) $ cd Question4
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question4 (main) $ javac four.java
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question4 (main) $ java four
+// 1 3 5 7 9 11 13 15 17 19 
