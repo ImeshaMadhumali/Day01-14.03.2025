5d3c4266e5274003d6ab78387966d9f221d06b4d
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2025-03-19 11:34:21 +0000

    Question3 completed

---
+++ Question3/three.java
@@ -0,0 +1,13 @@
+public class three{
+    public static void main(String[] args){
+        for(int i=10; i>=1; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
+
+// Output
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025 (main) $ cd Question3
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question3 (main) $ javac three.java
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question3 (main) $ java three
+// 10 9 8 7 6 5 4 3 2 1
