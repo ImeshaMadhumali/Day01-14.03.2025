32b8f0ef438f8ff34ff6333cf7286d7f665e909f
Imesha Madhumali <139534200+ImeshaMadhumali@users.noreply.github.com>
2025-03-19 01:11:58 +0000

    Question1 completed

---
+++ Question2/two.java
@@ -0,0 +1,13 @@
+public class two {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+// Output
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025 (main) $ cd Question2
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question2 (main) $ javac two.java
+// @ImeshaMadhumali ➜ /workspaces/Day01-14.03.2025/Question2 (main) $ java two
+// 1 2 3 4 5 6 7 8 9 10
